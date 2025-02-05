63567d46c73ef5dcf28c2ad9fb3cd1cc0a23edad
Rafael Moraes <rafaelmoraes005@gmail.com>
2025-02-05 19:22:19 -0300

    [initial commit]: Estrutura SpringBoot

---
+++ src/main/java/com/rsfinance/rsfinance/RsfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.rsfinance.rsfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsfinanceApplication.class, args);
+	}
+
+}
